1f9cc6270f29308f80f43627cce0e16a28587054
karthink <karthikchikmagalur@gmail.com>
2014-09-05 15:10:20 +0530

    Added 'provide.

--- lisp/setup-dired.el
+++ lisp/setup-dired.el
@@ -1,1 +1,14 @@
-(require 'dired-details+)
+(add-hook 'dired-mode-hook
+          (function (lambda ()
+                      ;; Set dired-x buffer-local variables here.
+                      ;; dired-details and dired-details+ add the
+                      ;; option to display only filenames in dired.
+                      ;; ")" to toggle
+                      (require 'dired-details+ nil t)
+                      ;; dired-x lets you jump to the directory the
+                      ;; current file is in with C-x C-j
+                      (require 'dired-x nil t)
+                      (setq dired-omit-mode 1)
+                      )))
+
+(provide 'setup-dired)

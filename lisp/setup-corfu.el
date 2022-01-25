eaebf58c8f585009021e8f3f5a4f2635e05480e4
Karthik Chikmagalur <karthikchikmagalur@gmail.com>
2022-01-24 23:10:32 -0800

    Add setup-corfu

---
+++ lisp/setup-corfu.el
@@ -0,0 +1,111 @@
+(use-package corfu
+  :ensure
+  ;; Optional customizations
+  ;; :custom
+  ;; (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+  ;; (corfu-auto t)                 ;; Enable auto completion
+  ;; (corfu-commit-predicate nil)   ;; Do not commit selected candidates on next input
+  ;; (corfu-quit-at-boundary t)     ;; Automatically quit at word boundary
+  ;; (corfu-quit-no-match t)        ;; Automatically quit if there is no match
+  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
+  ;; (corfu-preselect-first nil)    ;; Disable candidate preselection
+  ;; (corfu-echo-documentation nil) ;; Disable documentation in the echo area
+  ;; (corfu-scroll-margin 5)        ;; Use scroll margin
+
+  ;; You may want to enable Corfu only for certain modes.
+  ;; :hook ((prog-mode . corfu-mode)
+  ;;        (shell-mode . corfu-mode)
+  ;;        (eshell-mode . corfu-mode))
+
+  ;; Recommended: Enable Corfu globally. This is recommended since dabbrev can
+  ;; be used globally (M-/).
+  :hook (((prog-mode text-mode tex-mode) . corfu-mode)
+         ((shell-mode eshell-mode) . my/corfu-shell-settings))
+  :bind (:map corfu-map
+         ("TAB" . corfu-next)
+         ([tab] . corfu-next)
+         ("S-TAB" . corfu-previous)
+         ([backtab] . corfu-previous)
+         ("M-." . corfu-show-location)
+         ("C-n" . nil)
+         ("C-p" . nil)
+         ("M-h" . nil)
+         ("C-h" . corfu-show-documentation))
+  :config
+  (setq corfu-auto  t
+        corfu-cycle t
+        corfu-quit-no-match t
+        corfu-preselect-first nil
+        corfu-scroll-margin 5)
+  (setq corfu-quit-at-boundary nil)
+  (defun my/corfu-shell-settings ()
+    (setq-local corfu-quit-at-boundary t
+                corfu-quit-no-match t
+                corfu-auto nil)
+    (corfu-mode)))
+
+(use-package kind-icon
+  :ensure t
+  :after corfu
+  :custom
+  (kind-icon-default-face 'corfu-default) ; to compute blended backgrounds correctly
+  :config
+  (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
+
+;; Add extensions
+(use-package cape
+  :ensure
+  :bind (("C-; i" . cape-ispell)
+         ("C-; e" . cape-line)
+         ("C-; f" . cape-file)
+         ("C-; /" . cape-dabbrev))
+  ;; Bind dedicated completion commands
+  ;; :bind (("C-c p p" . completion-at-point) ;; capf
+  ;;        ("C-c p t" . complete-tag)        ;; etags
+  ;;        ("C-c p d" . cape-dabbrev)        ;; or dabbrev-completion
+  ;;        ("C-c p f" . cape-file)
+  ;;        ("C-c p k" . cape-keyword)
+  ;;        ("C-c p s" . cape-symbol)
+  ;;        ("C-c p a" . cape-abbrev)
+  ;;        ("C-c p i" . cape-ispell)
+  ;;        ("C-c p l" . cape-line)
+  ;;        ("C-c p w" . cape-dict)
+  ;;        ("C-c p \\" . cape-tex)
+  ;;        ("C-c p &" . cape-sgml)
+  ;;        ("C-c p r" . cape-rfc1345))
+  ;; :hook (text-mode . my/cape-text-mode-capfs)
+  :init
+  ;; Add `completion-at-point-functions', used by `completion-at-point'.
+  (add-to-list 'completion-at-point-functions #'cape-file)
+  (add-to-list 'completion-at-point-functions #'cape-tex)
+  ;; (add-to-list 'completion-at-point-functions #'cape-dabbrev)
+  ;; (add-to-list 'completion-at-point-functions #'cape-keyword)
+  ;;(add-to-list 'completion-at-point-functions #'cape-sgml)
+  ;;(add-to-list 'completion-at-point-functions #'cape-rfc1345)
+  ;;(add-to-list 'completion-at-point-functions #'cape-abbrev)
+  ;;(add-to-list 'completion-at-point-functions #'cape-ispell)
+  ;; (add-to-list 'completion-at-point-functions #'cape-dict)
+  ;;(add-to-list 'completion-at-point-functions #'cape-symbol)
+  ;;(add-to-list 'completion-at-point-functions #'cape-line)
+  :config
+  (setq cape-dict-file "/usr/share/dict/words")
+  (defun my/cape-text-mode-capfs ()
+    (add-to-list 'completion-at-point-functions #'cape-dict))
+  
+  (use-package pcomplete
+    :disabled
+    :defer
+    :config
+    ;; Silence the pcomplete capf, no errors or messages!
+    (advice-add 'pcomplete-completions-at-point :around #'cape-wrap-silent)
+    ;; (advice-remove 'pcomplete-completions-at-point #'cape-wrap-silent)
+
+    ;; Ensure that pcomplete does not write to the buffer
+    ;; and behaves as a pure `completion-at-point-function'.
+    (advice-add 'pcomplete-completions-at-point :around #'cape-wrap-purify)
+    ;; (advice-remove 'pcomplete-completions-at-point #'cape-wrap-purify)
+))
+
+(provide 'setup-corfu)
+;;; setup-corfu.el ends here
+

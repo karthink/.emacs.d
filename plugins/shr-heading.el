869f9a2d26e4512cc7658f32de958ddbb73b1a16
Karthik Chikmagalur <karthikchikmagalur@gmail.com>
2023-10-22 21:46:52 -0700

    shr-heading: Count <h5> as a shr heading

--- plugins/shr-heading.el
+++ plugins/shr-heading.el
@@ -2,7 +2,7 @@
 
 ;; From https://github.com/oantolin/emacs-config/blob/master/my-lisp/shr-heading.el
 (defun shr-heading-next (&optional arg)
-  "Move forward by ARG headings (any h1-h4).
+  "Move forward by ARG headings (any h1-h5).
 If ARG is negative move backwards, ARG defaults to 1."
   (interactive "p")
   (unless arg (setq arg 1))
@@ -13,7 +13,7 @@ If ARG is negative move backwards, ARG defaults to 1."
                 (funcall (if (> arg 0)
                              #'text-property-search-forward
                            #'text-property-search-backward)
-                         'face '(shr-h1 shr-h2 shr-h3 shr-h4)
+                         'face '(shr-h1 shr-h2 shr-h3 shr-h4 shr-h5)
                          (lambda (tags face)
                            (cl-loop for x in (if (consp face) face (list face))
                                     thereis (memq x tags))))))
@@ -25,7 +25,7 @@ If ARG is negative move backwards, ARG defaults to 1."
     (point)))
 
 (defun shr-heading-previous (&optional arg)
-  "Move backward by ARG headings (any h1-h4).
+  "Move backward by ARG headings (any h1-h5).
 If ARG is negative move forwards instead, ARG defaults to 1."
   (interactive "p")
   (shr-heading-next (- (or arg 1))))
@@ -35,7 +35,7 @@ If ARG is negative move forwards instead, ARG defaults to 1."
   (buffer-substring (line-beginning-position) (line-end-position)))
 
 (defun shr-heading-setup-imenu ()
-  "Setup imenu for h1-h4 headings in eww buffer.
+  "Setup imenu for h1-h5 headings in eww buffer.
 Add this function to appropriate major mode hooks such as
 `eww-mode-hook' or `elfeed-show-mode-hook'."
   (setq-local

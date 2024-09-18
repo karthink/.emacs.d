f70730fa9e8cf27aef066b4ffccdc9a5590d744e
Karthik Chikmagalur <karthikchikmagalur@gmail.com>
2024-09-18 14:17:33 -0700

    Calc: Better embedded mode binding
    - Remove calctex config
    - Misc calc customization

--- lisp/setup-calc.el
+++ lisp/setup-calc.el
@@ -1,14 +1,23 @@
 (use-package calc
   :bind (("C-x c" . calc)
-         ("H-S-c" . calc)
-         ("H-*" . calc-dispatch)
-         ("C-S-e" . latex-math-from-calc))
+         ("C-S-e" . latex-math-from-calc)
+         ("C-c e" . calc-embedded))
   :config
+  (use-package calc-embed
+    :bind (:map calc-override-minor-modes-map
+           ("'" . calc-algebraic-entry)))
+  (use-package calc-yank
+    :defer
+    :config
+    (define-advice calc-finish-stack-edit (:around (orig-fn &rest args) pop-to-buffer)
+      (cl-letf (((symbol-function 'switch-to-buffer)
+                 #'pop-to-buffer))
+        (apply orig-fn args))))
   (setq calc-make-windows-dedicated t)
   (defun latex-math-from-calc ()
     "Evaluate `calc' on the contents of line at point."
     (interactive)
-    (let ((lang (if (member major-mode '(org-mode latex-mode))
+    (let ((lang (if (memq major-mode '(org-mode latex-mode LaTex-mode))
                   'latex 'normal)))
       (cond ((region-active-p)
              (let* ((beg (region-beginning))
@@ -25,39 +34,17 @@
                                       calc-prefer-frac t
                                       calc-angle-mode rad)))))))))
 
-(use-package calctex
-  :disabled
-  :ensure (:host github :repo "johnbcoughlin/calctex"
-           :files ("*.el" "vendor" "org-calctex/*.el"
-                   "calctex/*.el" "calctex-contrib/*.el"))
-  :after calc
+(use-package calc
+  :after org-latex-preview
+  :hook (calc-mode . my/olp-calc-settings)
   :config
-  (setq calctex-additional-latex-packages "
-\\usepackage[usenames]{xcolor}
-\\usepackage{soul}
-\\usepackage{adjustbox}
-\\usepackage{amsmath}
-\\usepackage{amssymb}
-\\usepackage{siunitx}
-\\usepackage{cancel}
-\\usepackage{mathtools}
-\\usepackage{mathalpha}
-\\usepackage{xparse}
-\\usepackage{arevmath}"
-        calctex-additional-latex-macros
-        (concat calctex-additional-latex-macros
-                "\n\\let\\evalto\\Rightarrow"))
-  (let ((vendor-folder (expand-file-name
-                        (concat (file-name-as-directory "straight")
-                                (file-name-as-directory straight-build-dir)
-                                (file-name-as-directory "calctex/vendor"))
-                        straight-base-dir)))
-    (setq calctex-dvichop-bin (concat vendor-folder "texd/dvichop")
-          calctex-dvichop-sty (concat vendor-folder "texd/dvichop")
-          calctex-imagemagick-enabled-p nil))
-  (unless (file-exists-p calctex-dvichop-bin)
-    (message "CalcTeX: Building dvichop binary")
-    (let ((default-directory (file-name-directory calctex-dvichop-bin)))
-      (compile "make"))))
+  (defun my/olp-calc-settings ()
+    (setq-local org-latex-preview-numbered nil
+                org-latex-preview-auto-ignored-commands
+                '(mwheel-scroll pixel-scroll-precision
+                  scroll-up-command scroll-down-command
+                  scroll-other-window scroll-other-window-down))))
+
+
 
 (provide 'setup-calc)

09a7b9237b188ed5b34ebd258243f1da42fb9406
Karthik Chikmagalur <karthikchikmagalur@gmail.com>
2025-06-13 16:47:59 -0700

    init: Improve hippie-expand with suffix overlap handling
    * init.el (hippie-expand-try-functions-list, hippie-expand): When
    using hippie-expand, avoid expanding a suffix that already exists
    at point in the buffer.  This fixes the duplication issue that's
    been a thorn in my side for decades.
    
    Fix provided by tecosaur:
    URL https://code.tecosaur.net/tec/emacs-config/commit/1e6e64991e

--- init.el
+++ init.el
@@ -3897,19 +3897,45 @@ _d_: subtree
 ;;;----------------------------------------------------------------
 ;; Supercharge the way hippie-expand behaves, expand as little as
 ;; possible
-(setq hippie-expand-try-functions-list 
-      '(try-expand-dabbrev-visible
-        try-expand-dabbrev
-        try-expand-dabbrev-all-buffers
-        try-complete-file-name-partially
-        try-complete-file-name
-        try-expand-all-abbrevs
-        try-expand-list
-        try-expand-line
-        try-expand-whole-kill
-        try-expand-dabbrev-from-kill
-        try-complete-lisp-symbol-partially
-        try-complete-lisp-symbol))
+(use-package hippie-exp
+  :preface
+  (setq hippie-expand-try-functions-list
+        '(try-expand-dabbrev-visible
+          try-expand-dabbrev
+          try-expand-dabbrev-all-buffers
+          try-complete-file-name-partially
+          try-complete-file-name
+          try-expand-all-abbrevs
+          try-expand-list
+          try-expand-line
+          try-expand-whole-kill
+          try-expand-dabbrev-from-kill
+          try-complete-lisp-symbol-partially
+          try-complete-lisp-symbol))
+  :config
+  ;; From https://code.tecosaur.net/tec/emacs-config/commit/1e6e64991e
+  (defun my/he-subst-suffix-overlap (ins rem)
+    "The longest suffix of the string INS that is a prefix of REM.
+This is intended to be used when INS is a newly inserted string and REM is the
+remainder of the line, to allow for handling potentially duplicated content."
+    (let ((len (min (length ins) (length rem))))
+      (while (and (> len 0)
+                  (not (eq t (compare-strings ins (- len) nil rem 0 len))))
+        (setq len (1- len)))
+      len))
+
+  (define-advice he-substitute-string (:filter-args (args) suffix-strip)
+    "Filter ARG list for `he-substitute-string', truncating duplicated suffix.
+ARGS is the raw argument list (STRING &optional TRANS-CASE)."
+    (pcase-let* ((`(,ins &optional ,trans-case) args)
+                 (rem (save-excursion
+                        (goto-char (marker-position he-string-end))
+                        (buffer-substring-no-properties
+                         (point) (line-end-position))))
+                 (ov (my/he-subst-suffix-overlap ins rem)))
+      (when (>= ov 0)
+        (setq ins (substring ins 0 (- (length ins) ov))))
+      (list ins trans-case))))
 
 ;;;----------------------------------------------------------------
 ;; ** COMPANY-MODE

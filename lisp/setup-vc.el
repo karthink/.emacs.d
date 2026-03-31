0715badd8f4e5b101904c0d6b28601e5e9d685e9
Karthik Chikmagalur <chikmagalur@google.com>
2026-03-30 19:21:11 -0700

    setup-forge: Split vc and forge
    setup-forge.el: Split forge and pr-review setup into a separate
    feature from setup-vc.

--- lisp/setup-vc.el
+++ lisp/setup-vc.el
@@ -1,6 +1,5 @@
 ;; -*- lexical-binding: t; -*-
 
-;; VC gets the short-shrift in the age of Magit.
 (use-package vc
   :defer
   :config
@@ -291,53 +290,6 @@ project, as defined by `vc-root-dir'."
   (setq git-commit-summary-max-length 54
         git-commit-use-local-message-ring t)
   (defun my/git-commit-fill () (setq fill-column 66)))
-
-(use-package orgit :ensure t :defer)
-
-;; (if IS-GUIX
-;;     (load-library "forge-autoloads")
-;;   (elpaca forge)
-;;   (elpaca orgit-forge))
-(use-package forge
-  :ensure t
-  :defer
-  :config
-  (remove-hook 'forge-post-mode-hook 'turn-on-flyspell)
-  (add-hook 'forge-post-mode-hook #'jinx-mode)
-  ;; FIXME `forge-bug-reference-setup' should do this but doesn't
-  (add-hook 'forge-post-mode-hook
-            (lambda () (add-hook 'completion-at-point-functions
-                            #'forge-topic-completion-at-point nil t)))
-  (auth-source-pass-enable)
-  (setq forge-bug-reference-remote-files nil
-        forge-database-file
-        (expand-file-name "forge-database.sqlite" user-cache-directory)
-        forge-owned-accounts '(("karthink"))))
-
-(use-package orgit-forge :ensure t :defer)
-
-(use-package git-link
-  :ensure t
-  :defer
-  :config
-  (defun git-link-tecosaur (hostname dirname filename branch commit start end)
-    (format "https://%s/%s/src/%s/%s"
-	    hostname
-	    dirname
-	    (if commit
-                (format "commit/%s" commit)
-              (format "branch/%s" branch))
-	    (concat filename
-                    (when start
-                      (concat "#"
-                              (if end
-                                  (format "L%s-%s" start end)
-                                (format "L%s" start)))))))
-  (setf (alist-get "git.tecosaur.net" git-link-remote-alist
-                   nil nil #'equal)
-        (list 'git-link-tecosaur)))
-
-(use-package pr-review :ensure t :defer)
 
 ;; Misc git functions
 (use-package emacs
